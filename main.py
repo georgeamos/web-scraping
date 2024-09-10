2220ae63a858fe708b5dadbc7a91a7b913fbb18e
Adebayo Olaonipekun <pekunmi@live.com>
2024-09-10 17:31:29 +0100

    Added feature to scrape meta description

--- main.py
+++ main.py
@@ -6,15 +6,19 @@ def fetch_data(url):
     response = requests.get(url)
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
-        return soup.title.text
+        title = soup.title.text
+        description = soup.find('meta', {'name': 'description'})
+        description_content = description['content'] if description else 'No description available'
+        return title, description_content
     else:
-        return None
+        return None, None
 
 
 if __name__ == "__main__":
-    url = "https://www.linkedin.com/"
-    title = fetch_data(url)
-    if title:
-        print(f"Title of the page: {title}")
+    url = "https://www.oracle.com/"
+    page_title, page_description = fetch_data(url)
+    if page_title:
+        print(f"Title of the page: {page_title}")
+        print(f"Description: {page_description}")
     else:
         print(f"Failed to retrieve data.")
